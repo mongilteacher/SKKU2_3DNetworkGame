51ded004ca95bb7a8281129dc323b3eefcef6a5e
mongilteacher <mongilteacher@gmail.com>
2026-02-23 12:42:35 +0900

    [Asset] 배경, 스카이박스, 캐릭터 에셋 추가
    RPG Tiny Hero Duo PBR Polyart
    https://assetstore.unity.com/packages/3d/characters/humanoids/rpg-tiny-hero-duo-pbr-polyart-225148
    
    Simple Low Poly Nature Pack
    https://assetstore.unity.com/packages/3d/environments/landscapes/simple-low-poly-nature-pack-157552
    
    Fantasy Skybox FREE
    https://assetstore.unity.com/packages/2d/textures-materials/sky/fantasy-skybox-free-18353

--- Assets/02.Scripts/Core/PhotonServerManager.cs
+++ Assets/02.Scripts/Core/PhotonServerManager.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
@@ -62,13 +63,33 @@ public class PhotonServerManager : MonoBehaviourPunCallbacks
     public override void OnJoinedRoom()
     {
         Debug.Log("룸 입장 완료!");
+        
+        Debug.Log($"룸: {PhotonNetwork.CurrentRoom.Name}");
+        Debug.Log($"플레이어 인원: {PhotonNetwork.CurrentRoom.PlayerCount}");
+        
+        // 룸에 입장한 플레이어 정보
+        Dictionary<int, Player> roomPlayers = PhotonNetwork.CurrentRoom.Players;
+        foreach (KeyValuePair<int, Player> player in roomPlayers)
+        {
+            Debug.Log($"{player.Value.NickName} : {player.Value.ActorNumber}");
+        }
     }
 
     // 랜덤방 입장에 실패하면 자동으로 호출되는 콜백 함수
     public override void OnJoinRandomFailed(short returnCode, string message)
     {
         Debug.Log($"랜덤 방 입장에 실패했습니다: {returnCode} - {message}");
+
+        // 랜덤 룸 입장에 실패하면.. 룸이 하나도 없는 것이니... 룸을 만들자!
         
+        // 룸 옵션 정의
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.MaxPlayers = 20;  // 룸 최대 접속자 수
+        roomOptions.IsVisible = true; // 로비에서 룸을 보여줄 것인지
+        roomOptions.IsOpen = true;    // 룸의 오픈 여부
+        
+        // 룸 만들기 
+        PhotonNetwork.CreateRoom("test", roomOptions);
     }
     
     // 방 입장에 실패하면 자동으로 호출되는 콜백 함수
