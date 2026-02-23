937777a34260132ca48e4002ab4b7d067316899c
mongilteacher <mongilteacher@gmail.com>
2026-02-23 13:19:17 +0900

    [Feat] 캐릭터 중력 & 점프

--- Assets/02.Scripts/Player/PlayerMoveAbility.cs
+++ Assets/02.Scripts/Player/PlayerMoveAbility.cs
@@ -23,9 +23,18 @@ public class PlayerMoveAbility : MonoBehaviour
     {
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
+
         
         Vector3 direction = new Vector3(h, 0, v);
         direction.Normalize();
+        
+        _yVeocity -= GRAVITY * Time.deltaTime;
+        direction.y = _yVeocity;
+
+        if (Input.GetKey(KeyCode.Space) && _characterController.isGrounded)
+        {
+            _yVeocity = JumpForce;
+        }
         
         _characterController.Move(direction * Time.deltaTime * MoveSpeed);
     }
